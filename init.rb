87847ea4ff97eb74724b2803c72a5f50ce7e42f6
Ryan Dy <ryan.dy@gmail.com>
2010-04-04 23:15:39 -0700

    updating user based security

--- init.rb
+++ init.rb
@@ -2,3 +2,7 @@ require 'user_based_security'
 ActionController::Base.class_eval do
   include UserBasedSecurity::ActionController
 end
+
+ActiveRecord::Base.class_eval do
+  include UserBasedSecurity::ActiveRecord
+end
